d0f0a15be30443afa48a58cd1be45b03d8feb0a6
Peter Keating <peter@moov2.com>
2018-05-02 14:55:50 +0100

    Improve arguments processor to handle duplicate args
    When using `msdeploy`, it's common to need to use multiple `-skip` arguments
    to ignore multiple directories from being deleted during a sync. This change
    will add support for specifying multiple parameters within the `msdeploy` npm
    task.

--- index.js
+++ index.js
@@ -5,23 +5,48 @@ const fs = require('fs');
 const spawn = require('child_process').spawn;
 const exec = require('child_process').exec;
 
-var deploy = function(args, options) {
-
+var deploy = function () {
 	var params = '',
-		index = 0;
-	
-	for(var value in options) {
-		// Check if the option passed from user
-		var key = options[value];
-		if(key === true) {
-			var argument = "-"+value+":"+args[index];
-			params = params+' '+argument;
-			index++;
+		param;
+
+	for (var i = 0; i < process.argv.length; i++) {
+		if (!process.argv[i].startsWith('--')) {
+			continue;
 		}
+
+		param = process.argv[i].replace('--', '');
+
+		params += '-' + param;
+
+		if (i + 1 >= process.argv.length) {
+			continue;
+		}
+		
+		if (param === 'source') {
+			params += ':' + processSource(process.argv[i + 1]) + ' ';
+			continue;
+		}
+
+		params += ':' + process.argv[i + 1] + ' ';
 	}
 
 	runMSDeploy(params);
 };
+
+/**
+ * Provide better source behaviour. Let MSDeploy work with relative content paths!
+ */
+function processSource(source) {
+	if (typeof (source) === "undefined" || source.indexOf("contentPath=") !== 0) {
+		return source;
+	}
+	var sourcePath = source.replace("contentPath=", "");
+	if (path.isAbsolute(sourcePath)) {
+		return source;
+	}
+	sourcePath = path.resolve(sourcePath);
+	return "contentPath=" + sourcePath;
+}
 
 /*
  * Get MS deploy path
@@ -30,30 +55,30 @@ var deploy = function(args, options) {
 */
 function getExePath() {
 
-	if(!process.env.ProgramFiles || !process.env["ProgramFiles(x86)"]) {
+	if (!process.env.ProgramFiles || !process.env["ProgramFiles(x86)"]) {
 		throw new Error("This script is only available on Windows environment.");
 	}
 
 	var relativeMsDeployPath = "IIS/Microsoft Web Deploy V3/msdeploy.exe",
-	    path64 = path.join(process.env.ProgramFiles,relativeMsDeployPath),
-	    path32 = path.join(process.env["ProgramFiles(x86)"],relativeMsDeployPath),
-	    msDeploy64Path, msDeploy32Path;
+		path64 = path.join(process.env.ProgramFiles, relativeMsDeployPath),
+		path32 = path.join(process.env["ProgramFiles(x86)"], relativeMsDeployPath),
+		msDeploy64Path, msDeploy32Path;
 
 	if (path64 != null) {
-		msDeploy64Path = path.resolve(path.join(process.env.ProgramFiles,relativeMsDeployPath));
+		msDeploy64Path = path.resolve(path.join(process.env.ProgramFiles, relativeMsDeployPath));
 		if (fs.existsSync(msDeploy64Path)) {
-	    	return msDeploy64Path;
-	  	}
+			return msDeploy64Path;
+		}
 	}
 
 	if (path32 != null) {
-		msDeploy32Path = path.resolve(path.join(process.env["ProgramFiles(x86)"],relativeMsDeployPath));
-	  	if (fs.existsSync(msDeploy32Path)) {
-	    	return msDeploy32Path;
-	  	}
+		msDeploy32Path = path.resolve(path.join(process.env["ProgramFiles(x86)"], relativeMsDeployPath));
+		if (fs.existsSync(msDeploy32Path)) {
+			return msDeploy32Path;
+		}
 	}
 
-	throw new Error("MSDeploy doesn't seem to be installed. Could not find msdeploy in \""+msDeploy64Path+"\" or \""+msDeploy32Path+"\". You can install it from http://www.iis.net/downloads/microsoft/web-deploy");
+	throw new Error("MSDeploy doesn't seem to be installed. Could not find msdeploy in \"" + msDeploy64Path + "\" or \"" + msDeploy32Path + "\". You can install it from http://www.iis.net/downloads/microsoft/web-deploy");
 }
 
 
@@ -64,13 +89,13 @@ function runMSDeploy(param) {
 
 	param = param || '';
 
-	var msDeployPath = '"'+getExePath()+'"';
-	exec(msDeployPath+' '+param, (error, stdout, stderr) => {
-	  	if (error) {
-	   		console.error(`exec error: ${error}`);
-	   		throw new Error('msdeploy.exe error. Incorrect params.');
-	  	}
-	  	console.log(`stdout: ${stdout}`);
+	var msDeployPath = '"' + getExePath() + '"';
+	exec(msDeployPath + ' ' + param, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			throw new Error('msdeploy.exe error. Incorrect params.');
+		}
+		console.log(`stdout: ${stdout}`);
 	});
 }
 
